a8fec03ffa2f59280e7a7fd794a96fb02ba52487
Vilius <alaunisvilius@gmail.com>
2023-08-03 13:33:21 +0300

    Added sound effects of buttons

--- IndietopiaIntern/Assets/Scripts/ButtonController.cs
+++ IndietopiaIntern/Assets/Scripts/ButtonController.cs
@@ -1,13 +1,18 @@
-using UnityEditor.Animations;
 using UnityEngine;
 
 // Open doors on button click
 public class ButtonController : MonoBehaviour
 {
     public Animator doorAnimator;
+    private AudioSource soundSource;
 
+    private void Start()
+    {
+        soundSource = GetComponent<AudioSource>();
+    }
     private void OnMouseDown()
     {
         doorAnimator.SetTrigger("OpenDoor");
+        soundSource.Play();
     }
 }
